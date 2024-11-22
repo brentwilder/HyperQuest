ce5c404fb83bcccdc5f5671e2fef2abbdc2d2392
Brent Wilder <brentonwilder1995@gmail.com>
2024-11-22 16:32:17 -0500

    rlsd

--- spectralSNR/methods.py
+++ spectralSNR/methods.py
@@ -3,6 +3,7 @@ import pandas as pd
 import geopandas as gpd
 from rasterio.mask import mask
 from skimage.util import view_as_blocks
+from sklearn.linear_model import LinearRegression
 
 from common import *
 
@@ -64,7 +65,7 @@ def geostatistical(raster, geometry, geometry_attribute):
 
 
 
-def lmlsd(data, block_size, nbins):
+def lmlsd(data, block_size, nbins=150):
     '''
     TODO
 
@@ -91,6 +92,7 @@ def lmlsd(data, block_size, nbins):
     for i in range(blocked_image.shape[0]):
         for j in range(blocked_image.shape[1]):
             ij = blocked_image[i,j,:]
+            ij = ij.reshape(ij.shape[0], -1) 
             ij = ij[:, ~np.isnan(ij).any(axis=0)]
             mu_local = np.nanmean(ij, axis=1)
             sigma_local = np.nanstd(ij, axis=1)
@@ -143,12 +145,94 @@ def lmlsd(data, block_size, nbins):
 
 
 
-def rlsd(raster, block_size):
+def rlsd(data, block_size, nbins):
     '''
     TODO
     
     '''
-    return
+    #Load user data
+    datacube = load_data(data)
+    wavelengths = datacube.bands.centers
+
+    # start dataframe
+    df = pd.DataFrame(data=wavelengths, column='Wavelength')
+
+    # convert datacube to numpy array
+    array = None
+
+    # block it 
+    array = pad_image(array, block_size)
+    blocked_image = view_as_blocks(array, (block_size, block_size, wavelengths))
+    
+    # loop through each block and compute mu local and sigmal local for all wavelengths
+    local_sigma_dict = {w: [] for w in wavelengths}
+    local_mu_dict = {w: [] for w in wavelengths}
+    
+    for i in range(blocked_image.shape[0]):
+        for j in range(blocked_image.shape[1]):
+            ij = blocked_image[i,j,:]
+            ij = ij.reshape(ij.shape[0], -1) 
+            ij = ij[:, ~np.isnan(ij).any(axis=0)]
+
+            # Skip empty blocks
+            if ij.size == 0:
+                continue
+
+            # Iterate through wavelengths, skipping first and last
+            for k in range(1, len(wavelengths) - 1):
+                #x* = a + bx(k-1) + cx(k+1)... 
+                X = np.vstack((ij[k - 1], ij[k + 1])).T 
+                y = ij[k]
+                reg = LinearRegression()
+                reg.fit(X, y)
+                y_pred = reg.predict(X)
+                variance = np.var(np.sum((y - y_pred)**2))
+                sigma_local = np.sqrt(variance)
+                mu_local = np.mean(y)
+
+                # Store values for this wavelength
+                local_sigma_dict[wavelengths[k]].append(sigma_local)
+                local_mu_dict[wavelengths[k]].append(mu_local)
+
+    # Aggregate results: Compute mean sigma and mu across all blocks for each wavelength
+    signal = []
+    noise = []
+
+    for w in wavelengths:
+        lsd_values = np.array(local_sigma_dict[w])
+        lmu_values = np.array(local_mu_dict[w])
+        
+        # Create bins based on min max
+        bin_min = np.nanmin(lsd_values)
+        bin_max = np.nanmax(lsd_values)
+        bin_edges = np.linspace(bin_min, bin_max, nbins+1)
+        
+        # Count blocks in bins
+        bin_counts, _ = np.histogram(lsd_values, bins=bin_edges)
+        
+        # Find the bin with the most blocks
+        max_bin_idx = np.argmax(bin_counts)
+        selected_bin_min = bin_edges[max_bin_idx]
+        selected_bin_max = bin_edges[max_bin_idx + 1]
+
+        # Get LSD values in the selected bin
+        selected_sd = lsd_values[(lsd_values >= selected_bin_min) & (lsd_values < selected_bin_max)]
+        selected_mu = lmu_values[(lsd_values >= selected_bin_min) & (lsd_values < selected_bin_max)]
+        
+        # Calculate means
+        noise_value = np.nanmean(selected_sd)
+        noise.append(noise_value)
+        signal_value = np.nanmean(selected_mu)
+        signal.append(signal_value)
+
+    # Compute SNR
+    signal = np.array(signal)
+    noise = np.array(noise)
+    snr = signal / noise
+
+    df['SNR'] = snr
+
+    return df
 
 
 
