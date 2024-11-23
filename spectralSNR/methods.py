38208df054dc29e0bbcf608bb00df3de116ca8ff
brentwilder <brentonwilder1995@gmail.com>
2024-11-23 17:19:32 -0500

    close but padding not quite right

--- spectralSNR/methods.py
+++ spectralSNR/methods.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
 import geopandas as gpd
-from spectral import *
-from rasterio.mask import mask
 from skimage.util import view_as_blocks
 from sklearn.linear_model import LinearRegression
+import rioxarray as rio
 
 from utils import *
 
@@ -12,45 +11,50 @@ from utils import *
 def homogeneous_area(data, geometry_path, geometry_attribute):
 
     """
-    Calculate Signal-to-Noise Ratio (SNR) for each region of interest (ROI) in the shapefile, 
-    based on the mean and standard deviation of pixel intensities from the raster data.
+    Signal-to-Noise Ratio (SNR) for each region of interest using basic homogeneous area.
+    This is the simplest method and should be used with extreme caution.
     
     Args:
-    data: Path to the hyperspectral envi data or ENVI data
+    data: Path to the hyperspectral envi data
     geometry_path: Path to the geometry (shp, json, etc).
     geometry_attribute: Attribute that uniquely identifies each region.
     
     Returns:
     A pandas DataFrame with SNR with respect to wavelengths
     """
-
+    
     #Load user data
-    datacube = load_data(data)
+    src = rio.open_rasterio(data)
+    wavelengths = read_center_wavelength(data)
     roi = gpd.read_file(geometry_path)
-    wavelengths = datacube.bands.centers
+    roi = roi.to_crs(src.rio.crs) #ensure correct CRS
 
     # start dataframe
     df = pd.DataFrame(data=wavelengths, columns=['Wavelength'])
 
     # Loop through each ROI in the shapefile
-    for _, region in roi.iterrows():
-        region_geometry = region['geometry']
-        region_id = region[geometry_attribute]
-        
+    for _, r in roi.iterrows():
+        region_id = r[geometry_attribute]
+
         # Mask raster with the current region's geometry
-        out_image, _ = mask(datacube, [region_geometry], crop=True)
-        flat_image = out_image.reshape(out_image.shape[0], -1)  # Flatten for each band
+        out = src.rio.clip([r['geometry']]).values
+        flat = out.reshape(out.shape[0], -1).T
 
         # Remove NoData values
-        flat_image = flat_image[:, ~np.isnan(flat_image).any(axis=0)]
+        flat = flat[flat[:, -1] != -9999]
 
         # Calculate mean (signal) and std (noise) for each band
-        mu = np.nanmean(flat_image, axis=1)
-        sigma = np.nanstd(flat_image, axis=1)
+        mu = np.nanmean(flat, axis=0)
+        sigma = np.nanstd(flat, axis=0)
         snr = mu / sigma
 
         # Add to dataframe
+        df[f'{region_id}_Signal'] = mu
+        df[f'{region_id}_Noise'] = sigma
         df[f'{region_id}_SNR'] = snr
+    
+    # set index
+    df = df.set_index('Wavelength')
 
     return df
 
@@ -74,35 +78,44 @@ def lmlsd(data, block_size, nbins=150):
     
     '''
     #Load user data
-    datacube = load_data(data)
-    wavelengths = datacube.bands.centers
+    array = rio.open_rasterio(data).values
+    wavelengths = read_center_wavelength(data)
 
     # start dataframe
     df = pd.DataFrame(data=wavelengths, columns=['Wavelength'])
 
-    # convert datacube to numpy array
-    array = datacube.open_memmap(writeable=False)
-    print('loaded!')
     # block it 
     array = pad_image(array, block_size)
-    blocked_image = view_as_blocks(array, (block_size, block_size, wavelengths))
-    
+
+    blocked_image = array.reshape((block_size,
+                                  array.shape[1]// block_size,
+                                  block_size,
+                                  array.shape[2] // block_size,
+                                  array.shape[0])).swapaxes(1, 2)
+
     # loop through each block and compute mu local and sigmal local for all wavelengths
     local_sigma_dict = {w: [] for w in wavelengths}
     local_mu_dict = {w: [] for w in wavelengths}
-    for i in range(blocked_image.shape[0]):
-        for j in range(blocked_image.shape[1]):
-            ij = blocked_image[i,j,:]
-            ij = ij.reshape(ij.shape[0], -1) 
-            ij = ij[:, ~np.isnan(ij).any(axis=0)]
-            mu_local = np.nanmean(ij, axis=1)
-            sigma_local = np.nanstd(ij, axis=1)
-            for k, w in enumerate(wavelengths):
-                local_sigma_dict[w].append(sigma_local[k])
-                local_mu_dict[w].append(mu_local[k])
+    for i in range(blocked_image.shape[2]):
+        for j in range(blocked_image.shape[3]):
+            ij = blocked_image[:,:,i,j,:]
+            ij = ij.reshape(-1, ij.shape[2])
+            print(ij.shape)
+            ij = ij[ij[:, 0] != -9999]
+            print(ij.shape)
+            if ij.shape[0] != block_size**2: #edge NaN case
+                pass
+            else:
+                mu_local = np.nanmean(ij, axis=0)
+                print(mu_local)
+                break
+                sigma_local = np.nanstd(ij, axis=0)
+                for k, w in enumerate(wavelengths):
+                    local_sigma_dict[w].append(sigma_local[k])
+                    local_mu_dict[w].append(mu_local[k])
 
     # bin 
-    signal, noise = binning(local_mu_dict, local_mu_dict, wavelengths)
+    signal, noise = binning(local_mu_dict, local_mu_dict, wavelengths, nbins)
 
     # compute ratio
     snr = signal / noise
@@ -121,7 +134,7 @@ def rlsd(data, block_size, nbins=150):
     
     '''
     #Load user data
-    datacube = load_data(data)
+    datacube = load_image(data)
     wavelengths = datacube.bands.centers
 
     # start dataframe
@@ -185,7 +198,7 @@ def ssdc(data, block_size, nbins=150):
     
     '''
     #Load user data
-    datacube = load_data(data)
+    datacube = load_image(data)
     wavelengths = datacube.bands.centers
 
     # start dataframe
--- spectralSNR/utils.py
+++ spectralSNR/utils.py
@@ -1,28 +1,6 @@
-from spectral import open_image
+import rasterio
 import numpy as np
-
-
-def load_data(data):
-    """
-    Helper function for loading hyperspectral data in the methods.
- 
-    Parameters:
-    data (str or object): Path to a file (envi), or path to an object (envi)
-    
-    Returns:
-    datacube (object): The loaded ENVI data cube.
-    """
-    if isinstance(data, str):
-        try:
-            data = open_image(data)
-            return data
-        except Exception as e:
-            print(f"Error loading file with Spectral package: {e}")
-            return None
-    elif isinstance(data, object):  
-        return data 
-    else:
-        raise TypeError("Provided data is not an object or path.")
+import re
 
 
 
@@ -32,18 +10,20 @@ def pad_image(image, block_size):
     """
     Pads the image to ensure the dimensions are divisible by block_size, using np.nan for padding.
     """
-    height, width, bands = image.shape
+    bands, height, width = image.shape
     
     # Calculate padding for height and width
     pad_height = (block_size - (height % block_size)) % block_size
     pad_width = (block_size - (width % block_size)) % block_size
-    
+
     # Apply padding with np.nan
     padded_image = np.pad(image, 
-                          ((0, pad_height), (0, pad_width), (0, 0)), 
+                          ((0, 0),
+                           (0, pad_height), 
+                           (0, pad_width)), 
                           mode='constant', 
-                          constant_values=0)
-    
+                          constant_values=-9999)
+
     return padded_image
 
 
@@ -88,3 +68,19 @@ def binning(local_sigma_dict, local_mu_dict, wavelengths, nbins):
     noise = np.array(noise)
 
     return signal, noise
+
+
+
+def read_center_wavelength(img_path):
+    '''
+    TODO
+    '''
+    wavelength = None
+    for line in open(f'{img_path}.hdr', 'r'):
+        if 'wavelength =' in line or 'wavelength=' in line: 
+            wavelength = re.findall("[+-]?\d+\.\d+", line)
+            wavelength = ','.join(wavelength)
+            wavelength = wavelength.split(',')
+            wavelength = np.array(wavelength)
+    
+    return wavelength
