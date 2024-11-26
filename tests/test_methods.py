5e54df67cd7a05f921dd2a6e5ac2fe617c847d6e
Brent Wilder <brentonwilder1995@gmail.com>
2024-11-26 08:53:09 -0500

    test passes

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -15,17 +15,19 @@ wavelengths = hyperquest.read_center_wavelengths(path)
 
 ## RLSD TEST
 signal, noise, snr = hyperquest.rlsd(path, block_size=block_size, 
-                                nbins=nbins, ncpus=ncpus, snr_in_db=False, output_all=True)
+                                     nbins=nbins, ncpus=ncpus, 
+                                     snr_in_db=False, output_all=True)
 plt.scatter(wavelengths, snr, color='black')
 
 ## HRDSDC TEST
 snr = hyperquest.hrdsdc(path, n_segments=n_segments, 
-                   compactness=compactness, n_pca=n_pca, ncpus=ncpus)
+                        compactness=compactness, n_pca=n_pca, ncpus=ncpus)
 plt.scatter(wavelengths, snr, color='orange')
 
 
 ## SSDC TEST
-snr = hyperquest.ssdc(path, block_size=block_size, nbins=nbins, ncpus=ncpus)
+snr = hyperquest.ssdc(path, block_size=block_size, 
+                      nbins=nbins, ncpus=ncpus)
 plt.scatter(wavelengths, snr, color='magenta')
 
 
