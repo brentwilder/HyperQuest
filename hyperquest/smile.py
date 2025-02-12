194cf6fe3a5bab889b0539d5eaa4607bc15624ab
brentwilder <brentonwilder1995@gmail.com>
2025-02-12 18:56:59 -0500

    improved smile methods

--- hyperquest/smile.py
+++ hyperquest/smile.py
@@ -34,20 +34,15 @@ def smile_metric(hdr_path, mask_waterbodies=True, no_data_value=-9999):
 
     # Mask no data values
     array[array <= no_data_value] = np.nan
-
-    # create an interior-mask for keeping only columns where it is not any NaN
-    # NOTE this may cause data loss if masking waterbodies
-    array_for_mask = np.nanmean(array[:,:,1], axis=0)
-    interior_ix = ~np.isnan(array_for_mask).any(axis=0)
   
     # get wavelengths
     w, fwhm, obs_time = read_hdr_metadata(hdr_path)
 
     # set up outputs
-    co2_mean_output = np.full(array.shape[1], fill_value=np.nan)
-    co2_std_ouput = np.full(array.shape[1], fill_value=np.nan)
-    o2_mean_output = np.full(array.shape[1], fill_value=np.nan)
-    o2_std_ouput = np.full(array.shape[1], fill_value=np.nan)
+    co2_mean = np.full(array.shape[1], fill_value=np.nan)
+    co2_std = np.full(array.shape[1], fill_value=np.nan)
+    o2_mean = np.full(array.shape[1], fill_value=np.nan)
+    o2_std = np.full(array.shape[1], fill_value=np.nan)
 
     #  first, ensure the wavelengths covered the span of o2 and co2 features
     if np.max(w) < 800:
@@ -66,14 +61,10 @@ def smile_metric(hdr_path, mask_waterbodies=True, no_data_value=-9999):
     fwhm_bar_o2 = np.nanmean([fwhm[o2_index], fwhm[o2_index+1]])
     o2_dband = (o2_b1 + o2_b2) / fwhm_bar_o2
 
-    # Compute cross-track (columnwise) means and standard deviation
-    o2_mean = np.nanmean(o2_dband, axis=0)
-    o2_std = np.nanstd(o2_dband, axis=0)
-
-    # only include interior columns (not allow all nan)
-    o2_mean_output[interior_ix] = o2_mean
-    o2_std_ouput[interior_ix] = o2_std
-
+    # Compute cross-track (columnwise) means and standard deviation (w/respect to camera)
+    o2_mean, o2_std = cross_track_stats(o2_dband)
+    o2_mean = o2_mean.flatten()
+    o2_std = o2_std.flatten()
 
     # likely has enough data to find CO2
     if np.max(w)>2100: 
@@ -81,11 +72,9 @@ def smile_metric(hdr_path, mask_waterbodies=True, no_data_value=-9999):
         co2_b2 = array[:, :, co2_index+1]
         fwhm_bar_co2 = np.nanmean([fwhm[co2_index], fwhm[co2_index+1]])
         co2_dband = (co2_b1 + co2_b2) / fwhm_bar_co2
-        co2_mean = np.nanmean(co2_dband, axis=0)
-        co2_std = np.nanstd(co2_dband, axis=0)
-        co2_mean_output[interior_ix] = co2_mean
-        co2_std_ouput[interior_ix] = co2_std
-
+        co2_mean, co2_std = cross_track_stats(co2_dband)
+        co2_mean = co2_mean.flatten()
+        co2_std = co2_std.flatten()
 
     return o2_mean, co2_mean, o2_std, co2_std
 
@@ -108,7 +97,7 @@ def nodd_o2a(hdr_path, path_to_rtm_output_csv, ncpus=1,rho_s=0.15, mask_waterbod
     array[array <= no_data_value] = np.nan
 
     # Average in down-track direction (reduce to 1 row)
-    array = np.nanmean(array, axis=0)
+    array,_ = cross_track_stats(array)
 
     # Get data from hdr
     w_sensor, fwhm, obs_time = read_hdr_metadata(hdr_path)
